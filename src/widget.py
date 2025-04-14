4a92ebb86e2977e8dc77e79ae68db1b31777d912
Вавуло Светлана <sveta250206@gmail.com>
2025-04-14 20:39:02 +0300

    стилизовала код через функцию black

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(type_and_number_of_the_card_or_account: str) -> str:
     """Маскирует номер карты/счета в строке формата"""
@@ -12,9 +13,10 @@ def mask_account_card(type_and_number_of_the_card_or_account: str) -> str:
         masked_number = get_mask_card_number(number)
     return f"{description} {masked_number}"
 
+
 def get_date(date_str: str) -> str:
     """Преобразует строку с датой из формата "ГГГГ-ММ-ДДTчч:мм:сс.микросекунды" в формат "ДД.ММ.ГГГГ".
     Кто так даты пишет что их надо аж в нормальный вид приводить?"""
-    date_part = date_str.split('T')[0]
-    year, month, day = date_part.split('-')
+    date_part = date_str.split("T")[0]
+    year, month, day = date_part.split("-")
     return f"{day}.{month}.{year}"
