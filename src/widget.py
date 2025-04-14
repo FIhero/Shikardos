21844642a152f896a4820ac0169a538b2b07f32c
Вавуло Светлана <sveta250206@gmail.com>
2025-04-14 19:57:10 +0300

    Короче я коммиты поздно начала создавать, здесь создан файл widget.py и добавлены две функции

---
+++ src/widget.py
@@ -0,0 +1,20 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+def mask_account_card(type_and_number_of_the_card_or_account: str) -> str:
+    """Маскирует номер карты/счета в строке формата"""
+    parts = type_and_number_of_the_card_or_account.split()
+    number = parts[-1]
+    description = " ".join(parts[:-1])
+
+    if description.lower().startswith("счет"):
+        masked_number = get_mask_account(number)
+    else:
+        masked_number = get_mask_card_number(number)
+    return f"{description} {masked_number}"
+
+def get_date(date_str: str) -> str:
+    """Преобразует строку с датой из формата "ГГГГ-ММ-ДДTчч:мм:сс.микросекунды" в формат "ДД.ММ.ГГГГ".
+    Кто так даты пишет что их надо аж в нормальный вид приводить?"""
+    date_part = date_str.split('T')[0]
+    year, month, day = date_part.split('-')
+    return f"{day}.{month}.{year}"
