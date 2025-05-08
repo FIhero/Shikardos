04d982fbc0eca5a5993628904d3392b3a39bb075
Вавуло Светлана <sveta250206@gmail.com>
2025-05-08 18:01:30 +0300

    подправила код

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 
-def filter_by_state(data, state="EXECUTED"):
     """Возвращает новый список словарей, содержащий только те словари, у которых ключ
     state соответствует указанному значению"""
     return [item for item in data if item.get("state") == state]
 
 
-def sort_by_date(data, reverse=True):
     """Возвращает новый список, отсортированный по дате"""
     return sorted(data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse)
